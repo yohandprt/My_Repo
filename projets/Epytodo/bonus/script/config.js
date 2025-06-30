bf6422e87194925b4cf41f34307adf513df19e99
Yohan Dupret <yohan.dupret@epitech.eu>
2025-06-30 11:44:59 +0200

    refactor: rename projects file

--- projets/Epytodo/bonus/script/config.js
+++
@@ -1,7 +0,0 @@
-// script variable 
-// export 
-
-// selectionne les éléments HTML
-export const form = document.getElementById("todo-form");
-export const input = document.getElementById("todo-input");
-export const list = document.getElementById("todo-list");
